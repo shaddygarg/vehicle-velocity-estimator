cb202c59f38023e343ff198bed754bb6631fb8d2
Swapnil Negi <swapnil.negi09@gmail.com>
2019-04-07 04:44:07 +0530

    Add velocity estimator

--- new_code.py
+++ new_code.py
@@ -35,6 +35,7 @@ _,frame = cap.read()
 while True:
         c = 0
         c1 = 0
+        ans = 0
 	while True:
 		_,frame = cap.read()
 		#-----Drawing Stuff on the Image
@@ -50,8 +51,8 @@ while True:
 
 		#-----Finding ROI and extracting Corners
 		frameGray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-		roi = frameGray[gV.top_left[0]:gV.bottom_right[0], gV.top_left[1]:gV.bottom_right[1]  ] #selecting roi
-		new_corners = cv2.goodFeaturesToTrack(roi,50,0.01,10) #find corners
+		roi = frameGray[ gV.top_left[0]:gV.bottom_right[0], gV.top_left[1]:gV.bottom_right[1] ] #selecting roi
+		new_corners = cv2.goodFeaturesToTrack(roi, 50, 0.01, 10) #find corners
 
 		#-----converting to complete image coordinates (new_corners)
 
@@ -59,12 +60,18 @@ while True:
 		new_corners[:,0,1] = new_corners[:,0,1] + gV.top_left[0]
 
 		#-----drawing the corners in the original image
+		r_add, c_add = 0,0
 		for corner in new_corners:
-			cv2.circle(frame, (int(corner[0][0]),int(corner[0][1])) ,5,(0,255,0))
+			cv2.circle(frame, (int(corner[0][0]),int(corner[0][1])) ,5, (0,255,0))
+			r_add = r_add + corner[0][1]
+			c_add = c_add + corner[0][0]
+
 
 		#-----old_corners and oldFrame is updated
 		oldFrameGray = frameGray.copy()
 		old_corners = new_corners.copy()
+		centroid_old_row = int(1.0*r_add/len(new_corners))
+		centroid_old_col = int(1.0*c_add/len(new_corners))
 
 		cv2.imshow('tracker',frame)
 
@@ -78,16 +85,17 @@ while True:
 	#Actual Tracking
 	while True:
 		#Now we have oldFrame, we can get new_frame, we have old corners and we can get new corners and update accordingly
-                flag=0
-                #read new frame and cvt to gray
+		flag=0
+
+        #read new frame and cvt to gray
 		ret,frame = cap.read()
 		frameGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-                #finding the new tracked points
+        #finding the new tracked points
 		new_corners, st, err = cv2.calcOpticalFlowPyrLK(oldFrameGray, frameGray, old_corners, None, **lk_params)
 
-		#---pruning far away points:
-		#first finding centroid
+		# ---pruning far away points:
+		# first finding centroid
 		r_add, c_add = 0,0
 		for corner in new_corners:
 			r_add = r_add + corner[0][1]
@@ -95,40 +103,43 @@ while True:
 		centroid_row = int(1.0*r_add/len(new_corners))
 		centroid_col = int(1.0*c_add/len(new_corners))
 
-                #draw centroid
-		cv2.circle(frame,(int(centroid_col), int(centroid_row)), 5, (255,0,0))
+        # draw centroid
+		cv2.circle(frame, (int(centroid_col), int(centroid_row)), 5, (255,0,0))
 
-                #add only those corners to new_corners_updated which are at a distance of 30 or lesse
+        # add only those corners to new_corners_updated which are at a distance of 30 or lesser
 		new_corners_updated = new_corners.copy()
 		tobedel = []
 		for index in range(len(new_corners)):
-			if findDistance(new_corners[index][0][1],new_corners[index][0][0],int(centroid_row),int(centroid_col)) > 90:
+			if findDistance(new_corners[index][0][1], new_corners[index][0][0], int(centroid_row), int(centroid_col)) > 90:
 				tobedel.append(index)
 		new_corners_updated = np.delete(new_corners_updated,tobedel,0)
 
-		#drawing the new points
-		c1 += 1
-
+		# drawing the new points
 		for corner in new_corners_updated:
-			cv2.circle(frame, (int(corner[0][0]),int(corner[0][1])) ,5,(0,255,0))
-                        if  int(corner[0][0])>319:
-                            flag=1
-                            break
+			cv2.circle(frame, (int(corner[0][0]),int(corner[0][1])), 5, (0, 255, 0))
+			if  int(corner[0][0])>319:
+				flag = 1
+				break
 
-		if len(new_corners_updated) < 10 or flag==1 or c1==25:
-			print 'OBJECT LOST, Reinitialize for tracking'
+		c1 += 1
+		if len(new_corners_updated)<10 or flag==1 or c1==25:
 			break
 
-		#finding the min enclosing circle
+		ans += findDistance(centroid_old_col, centroid_old_row, centroid_col, centroid_row)
+
+		# finding the min enclosing circle
 		ctr , rad = cv2.minEnclosingCircle(new_corners_updated)
 
-		cv2.circle(frame, (int(ctr[0]),int(ctr[1])), int(rad), (0,0,255), thickness = 5)
+		cv2.circle(frame, (int(ctr[0]), int(ctr[1])), int(rad), (0, 0, 255), thickness = 5)
 
-		#updating old_corners and oldFrameGray
+		# updating old_corners and oldFrameGray
 		oldFrameGray = frameGray.copy()
 		old_corners = new_corners_updated.copy()
+		centroid_old_row = centroid_row
+		centroid_old_col = centroid_col
 
-		cv2.imshow('tracker',frame)
+
+		cv2.imshow('tracker', frame)
 
 		a = cv2.waitKey(50)
 		if a== 27:
@@ -136,5 +147,6 @@ while True:
 			cap.release()
 		elif a == 97:
 			break
+	print "The average speed of the car was {}".format(ans/c1-1)
 
 cv2.destroyAllWindows()
